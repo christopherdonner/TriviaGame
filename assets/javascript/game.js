d75c61c5c3e0b952ee940698cc5a4fbabc7045bb
Christopher Donner <christopher.donner@gmail.com>
2018-12-11 16:34:07 -0500

    @work-11/12/2018@16:34

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,28 @@
 var questionTimoeout = 60;
+var timer = 
+{
+    time: 0,
+    intervalID: 0,
+    clockRunning: false,
+    start: function()
+    {
+        if(!clockRunning)
+        {
+            intervalID=setInterval(timer.count, 1000);
+            clockRunning=true;
+        }
+    },
+    stop: function()
+    {
+        clearInterval(intervalID);
+        clockRunning=false;
+    },
+    count: function()
+    {
+        timer.time++;
+    }
+}
+
 var questionList=[question0 ,question1, question2, question3, question4, question5, question6, question7, question8, question9];
 var question0 = 
 {
@@ -81,4 +105,22 @@ function questionExpires()
 
 }
 
-setTimeout(questionExpires, 1000*questionTimoeout)
+function countDown()
+{
+    var timerInterval
+}
+
+window.onload = function() {
+
+$("#startButton").on("click", function(){
+    console.log("start")
+    //choose question at random
+    //display currentQuestion
+    //start 60 second timeout
+    //start displaying countdown
+})
+
+setInterval(timer.count, 1000)
+timer.count--;
+$("#timer").text("60")
+}
