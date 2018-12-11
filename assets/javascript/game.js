4a472d9f45720309e630faa8ce52edeb080c0859
christopherdonner <christopher.donner@gmail.com>
2018-12-11 08:19:25 -0500

    @home 11/12/2018

---
+++ assets/javascript/game.js
@@ -0,0 +1,76 @@
+var questionTimoeout = 60;
+var questionList=[question0 ,question1, question2, question3, question4, question5, question6, question7, question8, question9];
+var question0 = 
+{
+    question: "",
+    possibleAnswers: [],
+    correctAnswer: [],
+    questionTimer: 60
+}
+var question1 = 
+{
+    question: "",
+    possibleAnswers: [],
+    correctAnswer: [],
+    questionTimer: 60
+}
+var question2 = 
+{
+    question: "",
+    possibleAnswers: [],
+    correctAnswer: [],
+    questionTimer: 60
+}
+var question3 = 
+{
+    question: "",
+    possibleAnswers: [],
+    correctAnswer: [],
+    questionTimer: 60
+}
+var question4 = 
+{
+    question: "",
+    possibleAnswers: [],
+    correctAnswer: [],
+    questionTimer: 60
+}
+var question5 = 
+{
+    question: "",
+    possibleAnswers: [],
+    correctAnswer: [],
+    questionTimer: 60
+}
+var question6 = 
+{
+    question: "",
+    possibleAnswers: [],
+    correctAnswer: [],
+    questionTimer: 60
+}
+var question7 = 
+{
+    question: "",
+    possibleAnswers: [],
+    correctAnswer: [],
+    questionTimer: 60
+}
+var question8 = 
+{
+    question: "",
+    possibleAnswers: [],
+    correctAnswer: [],
+    questionTimer: 60
+}
+var question9 = 
+{
+    question: "",
+    possibleAnswers: [],
+    correctAnswer: [],
+    questionTimer: 60
+}
+
+
+
+
