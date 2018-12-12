8be76027d539a09e315fa89a9293b4e305eace41
Christopher Donner <christopher.donner@gmail.com>
2018-12-12 11:55:24 -0500

    @work 12/12/2018@11:55

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var questionTimoeout = 60;
 var timer = 
 {
-    time: 0,
+    time: 60,
     intervalID: 0,
     clockRunning: false,
     start: function()
@@ -19,10 +19,15 @@ var timer =
     },
     count: function()
     {
-        timer.time++;
+        timer.time--;
+    },
+    display: function()
+    {
+        $("#timer").text(timer.time)
     }
 }
 
+var currentQuestion
 var questionList=[question0 ,question1, question2, question3, question4, question5, question6, question7, question8, question9];
 var question0 = 
 {
@@ -98,29 +103,43 @@ var question9 =
 function pickQuestion()
 {
     //random number 0-9
+    var randomInt=Math.round(Math.random() *10)
+    console.log(randomInt)
+    currentQuestion=questionList[randomInt]
 }
 
+function drawQuestion(){
+
+}
 function questionExpires()
 {
 
 }
 
-function countDown()
-{
-    var timerInterval
-}
+
 
 window.onload = function() {
 
 $("#startButton").on("click", function(){
     console.log("start")
+    //timer.start
+    //timer.start()
+    setInterval(function(){timer.time--}, 1000)
+    $("#timer").text(timer.time)
+    //setInterval(timer.count, 1000)
     //choose question at random
+    pickQuestion()
     //display currentQuestion
+    drawQuestion()
     //start 60 second timeout
+    setTimeout(questionExpires, 60000)
     //start displaying countdown
 })
 
-setInterval(timer.count, 1000)
-timer.count--;
-$("#timer").text("60")
+while (timer.clockRunning){
+    timer.display
+    timer.count
+}
+setInterval(function(){timer.time--}, 1000)
+
 }
