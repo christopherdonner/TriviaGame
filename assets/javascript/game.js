3eda2dc0ae7b588071e58749ce0daede64d0a9a4
christopherdonner <christopher.donner@gmail.com>
2018-12-12 20:15:31 -0500

    @home 12/12/2018@20:15

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,145 +1,132 @@
-var questionTimoeout = 60;
-var timer = 
-{
-    time: 60,
-    intervalID: 0,
-    clockRunning: false,
-    start: function()
-    {
-        if(!clockRunning)
-        {
-            intervalID=setInterval(timer.count, 1000);
-            clockRunning=true;
-        }
-    },
-    stop: function()
-    {
-        clearInterval(intervalID);
-        clockRunning=false;
-    },
-    count: function()
-    {
-        timer.time--;
-    },
-    display: function()
-    {
-        $("#timer").text(timer.time)
-    }
-}
+var questionTimoeout = 10;
+var clockRunning=false
+var count=questionTimoeout
+var questionExpired=false
+var currentQuestion=""
 
-var currentQuestion
-var questionList=[question0 ,question1, question2, question3, question4, question5, question6, question7, question8, question9];
 var question0 = 
 {
-    question: "",
-    possibleAnswers: [],
+    question: "question0",
+    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: [],
-    questionTimer: 60
 }
 var question1 = 
 {
-    question: "",
-    possibleAnswers: [],
+    question: "question1",
+    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: [],
-    questionTimer: 60
 }
 var question2 = 
 {
-    question: "",
-    possibleAnswers: [],
+    question: "question2",
+    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: [],
-    questionTimer: 60
 }
 var question3 = 
 {
-    question: "",
-    possibleAnswers: [],
+    question: "question3",
+    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: [],
-    questionTimer: 60
 }
 var question4 = 
 {
-    question: "",
-    possibleAnswers: [],
+    question: "question4",
+    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: [],
-    questionTimer: 60
 }
 var question5 = 
 {
-    question: "",
-    possibleAnswers: [],
+    question: "question5",
+    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: [],
-    questionTimer: 60
 }
 var question6 = 
 {
-    question: "",
-    possibleAnswers: [],
+    question: "question6",
+    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: [],
-    questionTimer: 60
 }
 var question7 = 
 {
-    question: "",
-    possibleAnswers: [],
+    question: "question7",
+    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: [],
-    questionTimer: 60
 }
 var question8 = 
 {
-    question: "",
-    possibleAnswers: [],
+    question: "question8",
+    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: [],
-    questionTimer: 60
 }
 var question9 = 
 {
-    question: "",
-    possibleAnswers: [],
+    question: "question9",
+    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: [],
-    questionTimer: 60
 }
+var questionList=[question0 ,question1, question2, question3, question4, question5, question6, question7, question8, question9];
 
+var questionObject = {
+    "question1": question1,
+    "question1": question2,
+    "question1": question3,
+    "question1": question4,
+    "question1": question5,
+    "question1": question6,
+    "question1": question7,
+    "question1": question8,
+    "question1": question9,
+    "question1": question0,
+    }
 function pickQuestion()
 {
     //random number 0-9
     var randomInt=Math.round(Math.random() *10)
     console.log(randomInt)
     currentQuestion=questionList[randomInt]
+    //console.log(questionList)
+    console.log(currentQuestion.question)
 }
 
-function drawQuestion(){
-
-}
-function questionExpires()
+function drawQuestion()
 {
-
+    $("#question").text(currentQuestion.question)
+    $("#possibleAnswer1").text(currentQuestion.possibleAnswers[0])
+    $("#possibleAnswer1").text(currentQuestion.possibleAnswers[1])
+    $("#possibleAnswer1").text(currentQuestion.possibleAnswers[2])
+    $("#possibleAnswer1").text(currentQuestion.possibleAnswers[3])
 }
-
-
 
 window.onload = function() {
 
 $("#startButton").on("click", function(){
-    console.log("start")
-    //timer.start
-    //timer.start()
-    setInterval(function(){timer.time--}, 1000)
-    $("#timer").text(timer.time)
-    //setInterval(timer.count, 1000)
+    console.log("start");
+    clockRunning=true;
+    console.log(clockRunning)
+    setInterval(function(){
+        count--;
+        $("#timer").text(count)
+    }, 1000);
+    $("#timer").text(count);
     //choose question at random
-    pickQuestion()
+    pickQuestion();
+    drawQuestion();
+    $("#timer").text(count);
     //display currentQuestion
-    drawQuestion()
+    //console.log(currentQuestion)
     //start 60 second timeout
-    setTimeout(questionExpires, 60000)
+    setTimeout(function ()
+        {
+            //questionExpired
+            questionExpired=true; 
+            alert("expired")
+            pickQuestion();
+        }, questionTimoeout*1000);
     //start displaying countdown
 })
+console.log(clockRunning)
 
-while (timer.clockRunning){
-    timer.display
-    timer.count
-}
-setInterval(function(){timer.time--}, 1000)
 
+//setInterval(function(){timer.time--}, 1000)
 }
+
