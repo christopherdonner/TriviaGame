90cc4c28add7aea83b0c4c78ab5ad2d3911cb6b1
Christopher Donner <christopher.donner@gmail.com>
2018-12-14 16:21:50 -0500

    @work EOD 14/12/2018

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,67 +4,79 @@ var count=questionTimoeout
 var expired=false
 var currentQuestion=""
 var alreadySeen=[]
+var correct=0
+var incorrect=0
 
 
 var question0 = 
 {
-    question: "question0",
-    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
+    question: "What is Sailor Moon's real name?",
+    possibleAnswers: ["Selena", "Serena", "Missy", "Sarah"],
     correctAnswer: 1,
+    alreadyUsed: false
 }
 var question1 = 
 {
-    question: "question1",
-    possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
+    question: "What are Sailor Moon's cats named?",
+    possibleAnswers: ["Lilu and Aphrodite", "Lunar and Astro", "Luna and Artemis", "answerD"],
     correctAnswer: 2,
+    alreadyUsed: false
 }
 var question2 = 
 {
     question: "question2",
     possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: 3,
+    alreadyUsed: false
 }
 var question3 = 
 {
     question: "question3",
     possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: 0,
+    alreadyUsed: false
 }
 var question4 = 
 {
     question: "question4",
     possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: 3,
+    alreadyUsed: false
 }
 var question5 = 
 {
     question: "question5",
     possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: 2,
+    alreadyUsed: false
 }
 var question6 = 
 {
     question: "question6",
     possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: 1,
+    alreadyUsed: false
 }
 var question7 = 
 {
     question: "question7",
     possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
-    correctAnswer: 0
+    correctAnswer: 0,
+    alreadyUsed: false
 }
 var question8 = 
 {
     question: "question8",
     possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: 3,
+    alreadyUsed: false
 }
 var question9 = 
 {
     question: "question9",
     possibleAnswers: ["answerA", "answerB", "answerC", "answerD"],
     correctAnswer: 2,
+    alreadyUsed: false
 }
 var questionList=[question0 ,question1, question2, question3, question4, question5, question6, question7, question8, question9];
 
@@ -96,6 +108,7 @@ function pickQuestion()
     var randomInt=Math.floor(Math.random() *10)
     console.log(randomInt)
     currentQuestion=questionList[randomInt]
+    questionList[randomInt].alreadyUsed=true
     console.log(currentQuestion.question)
     expired=false
     alreadySeen.push(currentQuestion)
@@ -113,7 +126,9 @@ function pickQuestion()
         }, questionTimoeout*1000);
 }
 
+function rightAnswer(){}
 
+function wrongAnswer(){}
 
 function questionExpired()
 {
@@ -140,19 +155,7 @@ $("#startButton").on("click", function(){
     pickQuestion();
     drawQuestion();
     $("#timer").text(count);
-    //display currentQuestion
-    //console.log(currentQuestion)
-    //start 60 second timeout
-    /*setTimeout(function ()
-        {
-            questionExpired()
-            //questionExpired=true; 
-            //alert("expired")
-            //pickQuestion();
-        }, questionTimoeout*1000);
-    //start displaying countdown*/
-})
-//console.log(clockRunning)
+
 
 $("#possibleAnswer0.possibleAnswers").on("click", function(){
     console.log("possibleAnswer0")
@@ -172,11 +175,12 @@ $("#possibleAnswer1.possibleAnswers").on("click", function(){
     if(currentQuestion.correctAnswer===1)
     {
         alert("correct!")
-        pickQuestion()
+        rightAnswer()
     }
     else
     {
         alert("wrong!")
+        wrongAnswer()
     }
 });
 
@@ -206,5 +210,5 @@ $("#possibleAnswer3.possibleAnswers").on("click", function(){
     }
 });
 //setInterval(function(){timer.time--}, 1000)
+})
 }
-
